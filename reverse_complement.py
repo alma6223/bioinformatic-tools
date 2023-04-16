569d36c458728c48a6d25779eaee325896998979
Albert Martín Moreno <124532577+alma6223@users.noreply.github.com>
2023-04-16 11:25:12 +0200

    Add files via upload

---
+++ reverse_complement.py
@@ -0,0 +1,42 @@
+class Reverse_complement:
+    def __init__(self, fasta):
+        """
+        :param fasta: fasta: 'fasta' file with the nucleotide sequence to convert.
+        """
+        self.fasta = fasta
+
+    def get_sequence(self):
+        """
+        :return: returns the nucleotide sequence of a 'fasta' file.
+        """
+        with open(self.fasta, 'r') as seq:
+            sequence = seq.read().upper()
+        return sequence
+
+    def reverse(self):
+        """
+        :return: returns the 3' 5' directionality nucleotide sequence.
+        """
+        return self.get_sequence()[::-1]
+
+    def complement(self):
+        """
+        :return: returns the complementary sequence of nucleotides.
+        """
+        complement = ''
+        for i in self.get_sequence():
+            if i == 'A':
+                complement += 'T'
+            elif i == 'T':
+                complement += 'A'
+            elif i == 'C':
+                complement += 'G'
+            elif i == 'G':
+                complement += 'C'
+        return complement
+
+    def reverse_complement(self):
+        """
+        :return: returns the reverse sequence of the complementary sequence of nucleotides.
+        """
+        return self.get_complement()[::-1]
